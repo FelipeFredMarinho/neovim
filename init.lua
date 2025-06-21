57f0d821fd18b050ca8147c62d4f0b34f89f869b
Felipe Marinho <felipe.fcamarinho@gmail.com>
2025-06-21 01:38:10 -0300

    Update configs

---
+++ init.lua
@@ -0,0 +1,15 @@
+vim.cmd("set ignorecase")
+vim.cmd("set hlsearch")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set scrolloff=12")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+require("config")
+-- require("plugins")
+
+vim.cmd("colorscheme industry")
+
---
+++ lua/config/macros.lua
@@ -0,0 +1,5 @@
+vim.keymap.set({ "n", "v" }, "<leader>9", "s()<Esc>P")
+vim.keymap.set({ "n", "v" }, "<leader>0", "s[ <Esc>pa ]<Esc>")
+vim.keymap.set({ "n", "v" }, "<leader>{", "s{ <Esc>pa }<Esc>")
+vim.keymap.set({ "n", "v" }, "<leader>\"", "s\"\"<Esc>P")
+
