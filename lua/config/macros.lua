571f13216645457816447d84a5e2173d93efd5ff
Felipe Marinho <felipe.fcamarinho@gmail.com>
2025-06-21 01:38:10 -0300

    Update configs

---
+++ lua/config/macros.lua
@@ -0,0 +1,9 @@
+vim.keymap.set("n", "<leader>9", "s()<Esc>P")
+vim.keymap.set("v", "<leader>9", "s()<Esc>P")
+vim.keymap.set("n", "<leader>0", "s[ <Esc>pa ]<Esc>")
+vim.keymap.set("v", "<leader>0", "s[ <Esc>pa ]<Esc>")
+vim.keymap.set("n", "<leader>{", "s{ <Esc>pa }<Esc>")
+vim.keymap.set("v", "<leader>{", "s{ <Esc>pa }<Esc>")
+vim.keymap.set("n", "<leader>\"", "s\"\"<Esc>P")
+vim.keymap.set("v", "<leader>\"", "s\"\"<Esc>P")
+
