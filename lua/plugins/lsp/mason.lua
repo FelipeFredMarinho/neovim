d402b30691cc45449b34e90fef1b4751a72506ad
Felipe Marinho <felipe.fcamarinho@gmail.com>
2025-06-29 22:15:17 -0300

    Add LSP

---
+++ lua/plugins/lsp/mason.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    VeryLazy = true,
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜ ",
+          package_uninstalled = "✗",
+        },
+      },
+    },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    VeryLazy = true,
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+--    opts = {
+--      ensure_installed = {
+--        "bashls",
+--        "clangd",
+--        "cssls",
+--        "gopls",
+--        "html",
+--        "lua_ls",
+--        "pyright",
+--        "rust_analyzer",
+--      },
+--    },
+  },
+}
