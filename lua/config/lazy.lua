aa570582c46447577e18d4e928a34a0f5ba90b14
Felipe Marinho <felipe.fcamarinho@gmail.com>
2025-07-05 12:32:56 -0300

    Add gitsigns

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -34,6 +34,7 @@ require("lazy").setup({
     { import = "plugins.nvim-treesitter" },
     { import = "plugins.lsp" },
     { import = "plugins.autocomplete.blink-cmp" },
+    { import = "plugins.gitsigns" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,8 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    numhl = true,
+    word_diff = true,
+  }
+}
+
