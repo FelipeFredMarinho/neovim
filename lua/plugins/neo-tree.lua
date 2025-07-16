7f304c66a3b4bbf6c1055ae52334098f7e7250ca
Felipe Marinho <felipe.fcamarinho@gmail.com>
2025-07-16 02:08:58 -0300

    Add neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,35 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  lazy = false,
+  priority = 980,
+  opts = {
+    --defaults = { file_ignore_patterns = { "node_modules" } },
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false
+      }
+    },
+    window = {
+      mappings = {
+        ["l"] = "open",
+        ["h"] = "close_node"
+      }
+    },
+    tabs_layout = "active"
+  },
+  init = function()
+    vim.defer_fn(function()
+      vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE", ctermbg = "NONE" })
+      vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE", ctermbg = "NONE" })
+      vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { bg = "NONE", ctermbg = "NONE" })
+      vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "NONE", ctermbg = "NONE" })
+    end, 0)
+  end
+}
+
