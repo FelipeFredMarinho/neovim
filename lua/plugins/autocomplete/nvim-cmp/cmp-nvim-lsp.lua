e30733027d7ec2ba797606ba5fd5f5de81cb6a40
Felipe Marinho <felipe.fcamarinho@gmail.com>
2025-08-09 23:02:58 -0300

    Add inlay hints: gopls, rust_analyzer, js/ts

--- lua/plugins/autocomplete/nvim-cmp/cmp-nvim-lsp.lua
+++ lua/plugins/autocomplete/nvim-cmp/cmp-nvim-lsp.lua
@@ -5,7 +5,21 @@ return {
       bashls = {},
       clangd = {},
       cssls = {},
-      gopls = {},
+      gopls = {
+        settings = {
+          gopls = {
+            hints = {
+              rangeVariableTypes = true,
+              parameterNames = true,
+              constantValues = true,
+              assignVariableTypes = true,
+              compositeLiteralFields = true,
+              compositeLiteralTypes = true,
+              functionTypeParameters = true,
+            }
+          }
+        }
+      },
       html = {},
       lua_ls = {
         settings = {
@@ -17,8 +31,70 @@ return {
         }
       },
       pyright = {},
-      rust_analyzer = {},
-      ts_ls = {},
+      rust_analyzer = {
+        settings = {
+          rust_analyzer = {
+            inlayHints = {
+              bindingModeHints = {
+                enable = false,
+              },
+              chainingHints = {
+                enable = true,
+              },
+              closingBraceHints = {
+                enable = true,
+                minLines = 25,
+              },
+              closureReturnTypeHints = {
+                enable = "never",
+              },
+              lifetimeElisionHints = {
+                enable = "never",
+                useParameterNames = false,
+              },
+              maxLength = 25,
+              parameterHints = {
+                enable = true,
+              },
+              reborrowHints = {
+                enable = "never",
+              },
+              renderColons = true,
+              typeHints = {
+                enable = true,
+                hideClosureInitialization = false,
+                hideNamedConstructor = false,
+              },
+            },
+          }
+        }
+      },
+      ts_ls = {
+        settings = {
+          javascript = {
+            inlayHints = {
+              includeInlayEnumMemberValueHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayVariableTypeHints = false,
+            },
+          },
+          typescript = {
+            inlayHints = {
+              includeInlayEnumMemberValueHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayVariableTypeHints = false,
+            },
+          },
+        },
+      },
     },
   },
   config = function(_, opts)
