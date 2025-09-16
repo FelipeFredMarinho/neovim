7c1dbafefb413d4abbf8608cbac44d39cbfdf2b2
Felipe Marinho <felipe.fcamarinho@gmail.com>
2025-09-16 03:13:24 -0300

    Add dap: c/c++, golang, python

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -38,6 +38,7 @@ require("lazy").setup({
     { import = "plugins.gitsigns" },
     { import = "plugins.autoclose" },
     --{ import = "plugins.ai.augment" },
+    { import = "plugins.dap" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
