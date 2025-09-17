8c74c5df2d5c6b6b29acab2d3264742cdb2606a6
Felipe Marinho <felipe.fcamarinho@gmail.com>
2025-09-17 13:44:30 -0300

    Rm rust_analyzer inlayHints; rustaceanvim conflicts

--- lua/plugins/autocomplete/nvim-cmp/cmp-nvim-lsp.lua
+++ lua/plugins/autocomplete/nvim-cmp/cmp-nvim-lsp.lua
@@ -31,44 +31,44 @@ return {
         }
       },
       pyright = {},
-      rust_analyzer = {
-        settings = {
-          rust_analyzer = {
-            inlayHints = {
-              bindingModeHints = {
-                enable = false,
-              },
-              chainingHints = {
-                enable = true,
-              },
-              closingBraceHints = {
-                enable = true,
-                minLines = 25,
-              },
-              closureReturnTypeHints = {
-                enable = "never",
-              },
-              lifetimeElisionHints = {
-                enable = "never",
-                useParameterNames = false,
-              },
-              maxLength = 25,
-              parameterHints = {
-                enable = true,
-              },
-              reborrowHints = {
-                enable = "never",
-              },
-              renderColons = true,
-              typeHints = {
-                enable = true,
-                hideClosureInitialization = false,
-                hideNamedConstructor = false,
-              },
-            },
-          }
-        }
-      },
+--      rust_analyzer = {
+--        settings = {
+--          rust_analyzer = {
+--            inlayHints = {
+--              bindingModeHints = {
+--                enable = false,
+--              },
+--              chainingHints = {
+--                enable = true,
+--              },
+--              closingBraceHints = {
+--                enable = true,
+--                minLines = 25,
+--              },
+--              closureReturnTypeHints = {
+--                enable = "never",
+--              },
+--              lifetimeElisionHints = {
+--                enable = "never",
+--                useParameterNames = false,
+--              },
+--              maxLength = 25,
+--              parameterHints = {
+--                enable = true,
+--              },
+--              reborrowHints = {
+--                enable = "never",
+--              },
+--              renderColons = true,
+--              typeHints = {
+--                enable = true,
+--                hideClosureInitialization = false,
+--                hideNamedConstructor = false,
+--              },
+--            },
+--          }
+--        }
+--      },
       ts_ls = {
         settings = {
           javascript = {
