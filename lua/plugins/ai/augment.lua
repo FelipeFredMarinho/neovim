50442c57c952c11f74a6ac5cdcd072cd706ca17e
Felipe Marinho <felipe.fcamarinho@gmail.com>
2025-10-15 18:32:20 -0300

    Add augment.vim

---
+++ lua/plugins/ai/augment.lua
@@ -0,0 +1,9 @@
+return {
+  "augmentcode/augment.vim",
+  keys = {
+    { "<leader>ai", "<cmd>Augment chat-toggle<CR>" },
+    { "<leader>aic", "<cmd>Augment chat<CR>" },
+    { "<leader>ain", "<cmd>Augment chat-new<CR>" },
+  },
+}
+
