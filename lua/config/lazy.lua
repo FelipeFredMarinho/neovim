4ac79182f951ccc19235a4c46e3c637fcfd0fd4d
Felipe Marinho <felipe.fcamarinho@gmail.com>
2025-10-15 18:32:20 -0300

    Add augment.vim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -36,6 +36,7 @@ require("lazy").setup({
     { import = "plugins.none-ls" },
     { import = "plugins.autocomplete.nvim-cmp" },
     { import = "plugins.gitsigns" },
+    --{ import = "plugins.ai.augment" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
---
+++ lua/plugins/ai/augment.lua
@@ -0,0 +1,9 @@
+return {
+  "augmentcode/augment.vim",
+  keys = {
+    { "<leader>ai", ":Augment chat-toggle<CR>" },
+    { "<leader>aic", ":Augment chat<CR>" },
+    { "<leader>ain", ":Augment chat-new<CR>" },
+  },
+}
+
